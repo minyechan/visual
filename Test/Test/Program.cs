24fafe82ac8a1c72c91dc94b750c9e72819e52e5
mz-myc <myc@mz.co.kr>
2016-02-04 14:55:11 +0900

    1111

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -17,7 +17,7 @@ namespace Test
             Console.WriteLine("test1111");
             Console.WriteLine("test");
             Console.WriteLine("안녕하세요~^0^");
-            Console.WriteLine("111111");
+            Console.WriteLine("1111");
         }
     }
 }
