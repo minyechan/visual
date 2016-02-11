943ba3dd675a06d06d2e4187ab73ea9b5b291e03
mz-myc <myc@mz.co.kr>
2016-02-11 19:54:24 +0900

    change master

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -17,6 +17,7 @@ namespace Test
             Console.WriteLine("test1111");
             Console.WriteLine("test");
             Console.WriteLine("안녕하세요~^0^");
+            Console.WriteLine("안녕히가세요~^0^");
         }
     }
 }
